b054e93adbdc243d3c4976afc70843e709bdce23
Ihor Susoi <ihor.susoi@gmail.com>
2023-01-03 16:03:47 +0200

    fix: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let a = n;
+    if(a<0)a=a*(-1);
+    a = a.toString();
+    a = a.split('');
+    a = a.reverse();
+    a = a.join('');
+    return a;
 }
